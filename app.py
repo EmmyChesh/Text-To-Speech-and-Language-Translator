933899687b57402b20a16ce41c60333f0ec38803
EmmyChesh <110698173+EmmyChesh@users.noreply.github.com>
2024-08-21 21:51:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,18 +21,18 @@ st.markdown("""
     .stTextInput>div>input {
         border-radius: 10px;
         border: 2px solid #4CAF50;
-        font-size: 20px; /* Increase font size */
+        font-size: 15px; /* Increase font size */
         height: 60px; /* Increase height */
     }
     .stTextInput>label {
-        font-size: 22px; /* Increase label font size */
+        font-size: 15px; /* Increase label font size */
     }
     .stSelectbox>div>div>div {
         border-radius: 10px;
         border: 2px solid #4CAF50;
     }
     .stSelectbox>label {
-        font-size: 22px; /* Increase label font size */
+        font-size: 15px; /* Increase label font size */
     }
     .stButton>button {
         background-color: #4CAF50;
